89dcb02003af60ed5059e4863830b3e1d4bc71ce
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2020-01-27 11:22:28 -0800

    Small Updates

--- titanic.R
+++ titanic.R
@@ -1,10 +1,5 @@
 # Titanic - 1/20/2020 - M. L. DeBusk-Lane
-library(tidyverse)
-library(tidymodels)
-library(tune)
-library(janitor)
-library(readxl)
-library(tidylog)
+?
 
 # read in training dataset
 train <- read_csv("train.csv") %>% clean_names() %>%
@@ -26,8 +21,10 @@ titanic_recipe <- train %>%
   step_corr(all_predictors()) %>%
   step_center(all_predictors(), -all_outcomes()) %>%
   step_scale(all_predictors(), -all_outcomes()) %>%
-  step_bagimpute(age, embarked_Q, embarked_S) %>%
+  step_bagimpute(age, fare, embarked_Q, embarked_S) %>%
   prep() 
+
+titanic_recipe
 
 # Test data
 test_prepped <- titanic_recipe %>%
@@ -43,6 +40,7 @@ folds <- vfold_cv(train, v = 10)
 xg <- boost_tree(
   mode = "classification",
   trees = tune(),
+  mtry = tune(),
   min_n = tune(),
   tree_depth = tune(),
   learn_rate = tune()) %>%
@@ -51,11 +49,15 @@ xg <- boost_tree(
 # check tunable parameters
 xg %>% tunable()
 
+# build grid out
 grid <-  expand.grid(
-  trees = c(50),
+  trees = c(1000, 2000),
+  mtry = c(2, 4),
   min_n = c(3, 5, 7),
   tree_depth = 100,
   learn_rate = c(0.1, 0.5, 0.9))
+
+grid
 
 # Workflow
 titanic_wf <- workflow() %>%
@@ -70,16 +72,35 @@ fits <- tune_grid(
   model = xg,
   resamples  = folds,
   grid = grid,
-  metrics  = metric_set(roc_auc)
-) 
+  metrics  = metric_set(roc_auc, kap, accuracy))
+
 # Mostly from (https://nyhackr.blob.core.windows.net/presentations/Totally_Tidy_Tuning_Tools-Max_Kuhn.pdf)
+# Also: https://www.kaggle.com/hansjoerg/glmnet-xgboost-and-svm-using-tidymodels#data-pre-processing-code
 collect_metrics(fits)
-show_best(fits)
-good_values <- select_best(fits, maximize = FALSE)
+show_best(fits, "kap", n = 10, maximize = TRUE)
+good_values <- select_best(fits, "kap", maximize = FALSE)
 new_recipe <- finalize_recipe(titanic_recipe, good_values)
 new_mod <- finalize_model(xg, good_values)
+xg_final <- fit(new_mod, survived ~ ., data = train_prepped)
 
-# How do I tune this thing? 
+# Take a look at feature importance (https://notast.netlify.com/post/explaining-predictions-boosted-trees-post-hoc-analysis-xgboost/)
+library(xgboost) 
+xgb.importance(model = xg_final$fit) %>% head()
+
+
+# Set up predition model with test data
+tit_xg_final_prediction <- predict(xg_final, test_prepped, type = "prob") %>%
+  bind_cols(predict(xg_final, test_prepped)) %>%
+  bind_cols(test) %>% 
+  select(passenger_id, .pred_class) %>%
+  rename(Survived = .pred_class) %>%
+  rename(PassengerId = passenger_id) %>%
+  glimpse()
+
+write_csv(tit_xg_final_prediction, "tit_xg_predict_v3.csv")
+
+
+# Stopped here 1/21/2020
 
 
 # xgboost it!
